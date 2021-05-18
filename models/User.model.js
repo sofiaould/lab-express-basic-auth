e3baddc08f93d82625200ea8b1bf940d374ee8ff
Tran Van <vanbaotran201194@gmail.com>
2021-05-18 19:48:46 +0200

    model created

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,21 @@
 // User model here
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username : {
+        type:String,
+        unique: true,
+        required: [true, 'Username is required']
+    },
+    hashedPassword : {
+        type:String,
+        unique: true,
+        required: [true, 'Password is required']
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+module.exports = model('User',UserSchema)
