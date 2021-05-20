46397270602f6b780c87de8d74a974c7cbb87820
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-05-20 21:32:04 +0200

    finish

--- app.js
+++ app.js
@@ -17,23 +17,25 @@ const debug = require("debug")(
 
 const app = express();
 
+////////////// require  pour trouver les sessions/////////////
+
 require('./configs/session.config')(app);
 // require database configuration
 require("./configs/db.config");
 
-// Middleware Setup
+////////// Middleware Setup///////////
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
+///////// Express View engine setup///////
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
-// default value for title local
+///////// default value for title local/////////
 app.locals.title = "Express - Generated with IronGenerator";
 
 const index = require("./routes/index.routes");
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,57 +3,91 @@
 const express = require("express");
 const User = require("../models/User.model");
 const router = express.Router();
-const bcrypt= require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const { render } = require("../app");
 const saltRounds = 10;
-const salt = bcrypt.genSaltSync(saltRounds)
+const salt = bcrypt.genSaltSync(saltRounds);
 
+
+//////////////// route pour sign up ////////////////
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
-
-router.post('/signup', (req, res, next) => {
+/// route post sign up /// 
+router.post("/signup", (req, res, next) => {
   const { username, password } = req.body;
 
-  const hashedPassword = bcrypt.hashSync(password, salt)
-  console.log(`Password hash: ${hashedPassword}`);  
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  console.log(`Password hash: ${hashedPassword}`);
 
-  User.create({
-      username: username,
-      hashedPassword:hashedPassword
-    })
-      .then(userFromDB => {
-        console.log('Newly created user is: ', userFromDB)
-        res.redirect('/profile',{user:userFromDB});
-      })
-      .catch(error => next(error));
-});
-router.get('/login',(req,res,next)=>{
-  res.render('auth/login')
-})
-router.post('/login',(req,res,next)=>{
-  console.log('req.session:',req.session)
-  const {username, password} = req.body;
-  //check si les 2 champs sont remplis
-  if(username === '' || password === ''){
-    res.render('auth/login',{errorMessage:'Merci de remplir les 2 champs'})
-    return
-  }
-  User.findOne({username:username})
-  .then(userFromDB=>{
-    if(!userFromDB){
-      res.render('auth/login',{errorMessage:'user non trouve'})
-      return
-    } else if (bcrypt.compareSync(password,userFromDB.hashedPassword)){
-      req.session.currentUser = userFromDB;
-      res.redirect('/profile')
-    } else {
-      res.render('auth/login',{errorMessage:'Mauvais mot de passe'})
-    }
+  User.create({    // creation de users
+    username: username,
+    hashedPassword: hashedPassword,
   })
-  .catch(err=>next(err))
-})
-router.get('/profile',(req,res,next)=>{
-  res.render('auth/profile',{user: req.session.currentUser})
-})
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      res.redirect("/login");
+    })
+    .catch((error) => next(error));
+});
+
+/////////////////// routes pour login ///////////////////////////
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+//// post pour login ////
+
+router.post("/login", (req, res, next) => {
+  console.log("req.session:", req.session);
+  const { username, password } = req.body;
+  //check si les 2 champs sont remplis
+  if (username === "" || password === "") {
+    res.render("auth/login", { errorMessage: "Merci de remplir les 2 champs" });
+    return;
+  }
+  User.findOne({ username: username })
+    .then((userFromDB) => {
+      if (!userFromDB) {
+        res.render("auth/login", { errorMessage: "user non trouve" });
+        return;
+      } else if (bcrypt.compareSync(password, userFromDB.hashedPassword)) {
+        req.session.currentUser = userFromDB;
+        res.redirect("/profile");
+      } else {
+        res.render("auth/login", { errorMessage: "Mauvais mot de passe" });
+      }
+    })
+    .catch((err) => next(err));
+});
+/////////////////////////////////// route vers le profil ///////////////////////////////////
+
+router.get("/profile", (req, res, next) => {
+  res.render("auth/profile", { user: req.session.currentUser });
+});
+
+router.get("/private", (req, res, next) => {
+  if (req.session.currentUser) {
+    res.render("auth/private");
+  } else {
+    res.render("auth/login", {
+      errorMessage: "Vous devez vous identifiez pour acceder a la page",
+    });
+  }
+});
+/////////////////// le route vers fichiers privées et public////////////////////////////
+
+router.get("/main", (req, res, next) => {
+  res.render("auth/main");
+});
+
+// ///////////////////////route vers log out////////////////////////////////
+
+router.post('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+
 module.exports = router;
